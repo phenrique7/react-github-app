99b8990600c51c2d6ae97cdaf3aacb82d6fe21d2
Paulo Henrique <pauloh1288@gmail.com>
2018-11-24 22:00:16 -0300

    feat: add sass along with css modules and hot reload

--- src/stories/index.jsx
+++ src/stories/index.jsx
@@ -6,7 +6,8 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module)
+  .add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello, Button</Button>)
