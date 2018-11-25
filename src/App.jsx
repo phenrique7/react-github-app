edb3af886d1618b57976cf9e275d40e7d01ed485
Paulo Henrique <pauloh1288@gmail.com>
2018-11-25 15:04:09 -0300

    feat: add blueprint UI toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import styles from './assets/css/sass/index.module.scss';
 
 function App() {
   return (
-    <div className={styles.app}>
+    <div>
       <p>
         This is a paragraph
       </p>
-      <a href="#">
-        This is a link
-      </a>
     </div>
   );
 }
