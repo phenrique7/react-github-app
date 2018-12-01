2563fff7d1f68ef77049c2b32ce6456ad93a3298
Paulo Henrique <pauloh1288@gmail.com>
2018-12-01 16:13:05 -0300

    feat: add recompose for repo listing

--- src/components/user-profile/RepoList.jsx
+++ src/components/user-profile/RepoList.jsx
@@ -1,81 +1,80 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
+import { compose, withHandlers, withState } from 'recompose';
 import matchSorter from 'match-sorter';
 import { Classes } from '@blueprintjs/core';
 import RepoCard from './RepoCard';
 import styles from '../../assets/css/sass/user-profile/repo-list.module.scss';
 
-class RepoList extends React.Component {
-  constructor(props) {
-    super(props);
+function RepoList(props) {
+  const { reposState: repos, handleFilterUpdate } = props;
 
-    const { repos } = this.props;
-
-    this.state = {
-      repos,
-      filter: '',
-    };
-
-    this.handleFilterUpdate = this.handleFilterUpdate.bind(this);
-  }
-
-  handleFilterUpdate({ target: { value } }) {
-    this.setState({ filter: value });
-  }
-
-  render() {
-    const { repos, filter } = this.state;
-    let matchingRepos = [...repos];
-
-    if (filter) {
-      matchingRepos = matchSorter(repos, filter, {
-        keys: [
-          'name',
-          {
-            maxRanking: matchSorter.rankings.SIMPLE_MATCH,
-            key: 'language',
-          },
-          {
-            maxRanking: matchSorter.rankings.CONTAINS,
-            key: 'description',
-          },
-        ],
-      });
-    }
-
-    return (
-      <div className={styles.repoListContainer}>
-        <div className={styles.repoListHeader}>
-          <h4>Repositories</h4>
-          <input
-            type="text"
-            className={Classes.INPUT}
-            placeholder="Find a  repository..."
-            dir="auto"
-            onChange={this.handleFilterUpdate}
-          />
-        </div>
-        <div className={styles.repoList}>
-          {matchingRepos.map(repo => (
-            <RepoCard
-              key={shortid.generate()}
-              repoName={repo.name}
-              repoUrl={repo.url}
-              repoDescription={repo.description}
-              repoLanguage={repo.language}
-              repoStars={repo.stars}
-              repoForks={repo.forks}
-            />
-          ))}
-        </div>
+  return (
+    <div className={styles.repoListContainer}>
+      <div className={styles.repoListHeader}>
+        <h4>Repositories</h4>
+        <input
+          type="text"
+          className={Classes.INPUT}
+          placeholder="Find a  repository..."
+          dir="auto"
+          onChange={handleFilterUpdate}
+        />
       </div>
-    );
-  }
+      <div className={styles.repoList}>
+        {repos.map(repo => (
+          <RepoCard
+            key={shortid.generate()}
+            repoName={repo.name}
+            repoUrl={repo.url}
+            repoDescription={repo.description}
+            repoLanguage={repo.language}
+            repoStars={repo.stars}
+            repoForks={repo.forks}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-RepoList.propTypes = {
-  repos: PropTypes.array.isRequired,
+RepoList.defaultProps = {
+  handleFilterUpdate: () => {},
 };
 
-export default RepoList;
+RepoList.propTypes = {
+  reposState: PropTypes.array.isRequired,
+  handleFilterUpdate: PropTypes.func,
+};
+
+export default compose(
+  withState(
+    'reposState',
+    'setRepos',
+    props => props.repos,
+  ),
+  withHandlers({
+    handleFilterUpdate: props => ({ target: { value } }) => {
+      let matchingRepos = [...props.repos];
+
+      if (value) {
+        matchingRepos = matchSorter(props.repos, value, {
+          keys: [
+            'name',
+            {
+              maxRanking: matchSorter.rankings.SIMPLE_MATCH,
+              key: 'language',
+            },
+            {
+              maxRanking: matchSorter.rankings.CONTAINS,
+              key: 'description',
+            },
+          ],
+        });
+      }
+
+      props.setRepos(matchingRepos);
+    },
+  }),
+)(RepoList);
